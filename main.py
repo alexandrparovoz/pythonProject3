d35b429100a7cf4328f557a6760b8d353eea1b9e
User <lonzo.trans@gmail.com>
2023-02-23 19:49:07 +0300

    parsing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ counter = 0
 while cur_page < max_page:
     reg = requests.get('https://realt.by/sale/flats/?page=1{cur_page}')
     ht = BeautifulSoup(reg.content,'html.parser')
+    prices = enumerate(ht.find_all('div', class_='text-truncate'))
     for counter, a in enumerate(ht.find_all('a', class_='teaser-title')):
         if 'news' in a['href'] or 'code' in a['href']:
             continue
